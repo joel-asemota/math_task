1d38fa93082cedb28846a79ad9166624e4617dc7
50566282 <50566282@CW-B207-1233S>
2024-01-16 16:23:03 +0000

    Add project files.

---
+++ math_task/math_task.py
@@ -0,0 +1,31 @@
+# finding square root of 8, 65, 78, 34 and 98
+import math 
+sqrt = math.sqrt(8)
+sqrt = math.sqrt(65)
+sqrt = math.sqrt(78)
+sqrt = math.sqrt(34)
+sqrt = math.sqrt(98)
+# finding power of 6^5
+power = math.pow(6, 5)
+# finding power of 7^8
+power = math.pow(7, 8)
+# finding power of 9^13
+power = math.pow(9, 13)
+# finding power of 23^4
+power = math.pow(23, 4)
+# finding power of 7^4
+power = math.pow(7, 4)
+
+# rounding numbers up
+ceil_val = math.ceil(6.5)
+ceil_val = math.ceil(7.3)
+ceil_val = math.ceil(8.6)
+ceil_val = math.ceil(8.9)
+ceil_val = math.ceil(54.763)
+
+# rounding numbers down
+floor_val = math.floor(2.7)
+floor_val = math.floor(6.3)
+floor_val = math.floor(8.5)
+floor_val = math.floor(87.4)
+floor_val = math.floor(87.45976)
